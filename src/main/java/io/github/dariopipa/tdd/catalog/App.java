8bac9d5fdefb15fc389362e9cd2f969e47ce6b58
user3306 <Dariopipa733@gmail.com>
2025-09-10 00:25:45 +0200

    Initl maven project setup

---
+++ src/main/java/io/github/dariopipa/tdd/catalog/App.java
@@ -0,0 +1,11 @@
+package io.github.dariopipa.tdd.catalog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/io/github/dariopipa/tdd/catalog/AppTest.java
@@ -0,0 +1,18 @@
+package io.github.dariopipa.tdd.catalog;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	/**
+	 * Rigorous Test :-)
+	 */
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+}
