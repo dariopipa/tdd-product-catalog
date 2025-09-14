a6d7fedd3042c1a5b267a96170357e4f5c64d9b9
user3306 <Dariopipa733@gmail.com>
2025-09-14 21:07:51 +0200

    move files to new package

---
+++ src/main/java/io/github/dariopipa/tdd/catalog/repository/ProductRepository.java
@@ -0,0 +1,19 @@
+package io.github.dariopipa.tdd.catalog.repository;
+
+import java.util.List;
+
+import io.github.dariopipa.tdd.catalog.entities.Product;
+
+public interface ProductRepository {
+	Long create(Product product);
+
+	void delete(Long id);
+
+	Product findById(Long id);
+
+	Product findByName(String name);
+
+	Product update(Product product);
+
+	List<Product> findAll();
+}
