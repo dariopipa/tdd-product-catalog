c37782372e21f16d459752b40c431bbd0c71bb4f
user3306 <Dariopipa733@gmail.com>
2025-09-20 17:59:51 +0200

    feat: add product controller unit test & add view interface to be mocked by the controller & add the controller implementaion aswell

---
+++ src/main/java/io/github/dariopipa/tdd/catalog/controllers/ProductController.java
@@ -0,0 +1,62 @@
+package io.github.dariopipa.tdd.catalog.controllers;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import io.github.dariopipa.tdd.catalog.entities.Product;
+import io.github.dariopipa.tdd.catalog.exceptions.EntityNotFoundException;
+import io.github.dariopipa.tdd.catalog.exceptions.ProductNameAlreadyExistsExcpetion;
+import io.github.dariopipa.tdd.catalog.service.ProductService;
+import io.github.dariopipa.tdd.catalog.views.ProductView;
+
+public class ProductController {
+
+	private ProductService productService;
+	private ProductView productView;
+
+	public ProductController(ProductService productService, ProductView productView) {
+		this.productService = productService;
+		this.productView = productView;
+	}
+
+	public void create(String name, BigDecimal price, Long categoryId) {
+		try {
+			Long id = productService.create(name, price, categoryId);
+			Product product = productService.findById(id);
+			productView.addedProduct(product);
+		} catch (ProductNameAlreadyExistsExcpetion e) {
+			productView.showError("Invalid input: Product name already exists");
+		} catch (Exception e) {
+			productView.showError("Invalid input: " + e.getMessage());
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			Product deletedProduct = productService.findById(id);
+			productService.delete(id);
+			productView.deletedProduct(deletedProduct);
+		} catch (EntityNotFoundException e) {
+			productView.showError(e.getMessage());
+		}
+	}
+
+	public void findAll() {
+		List<Product> products = productService.findAll();
+		productView.findAll(products);
+	}
+
+	public void update(Long id, String newName, BigDecimal valueOf, Long categoryId) {
+
+		try {
+			Product updatedProduct = productService.update(id, newName, valueOf, categoryId);
+			productView.updateProduct(updatedProduct);
+		} catch (IllegalArgumentException e) {
+			productView.showError("Invalid input: " + e.getMessage());
+		} catch (Exception e) {
+			productView.showError("Invalid input: " + e.getMessage());
+		}
+
+	}
+
+}
